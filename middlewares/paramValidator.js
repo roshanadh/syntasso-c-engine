eebb607d1c930eee9047dc9a18a040f935d90986
Roshan <nahsorad@gmail.com>
2020-07-22 21:43:36 +0545

    Validate req.body.code and req.body.dockerConfig

---
+++ middlewares/paramValidator.js
@@ -0,0 +1,9 @@
+module.exports.codeValidator = req => (req.body.code ? true : false);
+
+module.exports.dockerConfigValidator = req => {
+	if (!req.body.dockerConfig) return "no-config";
+	else if (isNaN(req.body.dockerConfig)) return "NaN";
+	else if (![0, 1, 2].includes(parseInt(req.body.dockerConfig)))
+		return "no-valid-config";
+	else return "ok";
+};
