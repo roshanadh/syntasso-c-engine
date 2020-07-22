549b8c63fd7895955ff7fecb27b368f9b4ed1e4e
Roshan <nahsorad@gmail.com>
2020-07-22 21:44:04 +0545

    Use submitController & dockerConfigController for /submit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 
+const submitController = require("../controllers/submitController.js");
+const dockerConfigController = require("../controllers/dockerConfigController.js");
+
 router.get("/", (req, res) => {
 	res.json({ message: "Hello World!" });
 });
+
+router.post("/submit", submitController, dockerConfigController);
 
 module.exports = router;
