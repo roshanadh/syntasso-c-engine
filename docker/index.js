3d4b1c6e3d94d38c9a4d40945bc9b3942f864d16
Roshan <nahsorad@gmail.com>
2020-07-22 21:42:15 +0545

    Export docker API endpoints

---
+++ docker/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	buildCImage: require("./buildCImage.js"),
+	createCContainer: require("./createCContainer.js"),
+	startCContainer: require("./startCContainer.js"),
+	compileInCContainer: require("./compileInCContainer.js"),
+	execInCContainer: require("./execInCContainer.js"),
+};
