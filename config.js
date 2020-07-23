bbec125a6fac9a3d14f644efc7e133534f4a4522
Roshan <nahsorad@gmail.com>
2020-07-23 19:23:44 +0545

    Export client's protocol, host, and port

--- config.js
+++ config.js
@@ -4,4 +4,7 @@ if (!process.env) throw new Error("Environment variable(s) not set.");
 
 module.exports = {
 	PORT: process.env.PORT || 8081,
+	CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
+	CLIENT_HOST: process.env.CLIENT_HOST,
+	CLIENT_PORT: process.env.CLIENT_PORT,
 };
