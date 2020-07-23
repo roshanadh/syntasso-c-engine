4225c8e919bc7dc23dc2bcc2be0c89b609f79331
Roshan <nahsorad@gmail.com>
2020-07-23 19:49:35 +0545

    Use redis store for express sessions

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const redis = require("redis");
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
 
 const router = require("./routes/index.js");
 const {
@@ -8,6 +11,9 @@ const {
 	CLIENT_PROTOCOL,
 	CLIENT_HOST,
 	CLIENT_PORT,
+	SECRET_SESSION_KEY,
+	REDIS_STORE_HOST,
+	REDIS_STORE_PORT,
 } = require("./config.js");
 
 const app = express();
@@ -16,6 +22,18 @@ app.use(
 	cors({
 		credentials: true,
 		origin: `${CLIENT_PROTOCOL}://${CLIENT_HOST}:${CLIENT_PORT}`,
+	})
+);
+app.use(
+	session({
+		secret: SECRET_SESSION_KEY,
+		store: new RedisStore({
+			host: REDIS_STORE_HOST,
+			port: parseInt(REDIS_STORE_PORT),
+			client: redis.createClient(),
+		}),
+		saveUninitialized: false,
+		resave: false,
 	})
 );
 app.use(bodyParser.urlencoded({ extended: false }));
