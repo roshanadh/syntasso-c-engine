2205076a1bd20f65bfdc5e9ddeadbaf1b1479ede
Roshan <nahsorad@gmail.com>
2020-07-23 23:02:53 +0545

    Export copyClientFilesToCContainer func

--- docker/index.js
+++ docker/index.js
@@ -2,6 +2,7 @@ module.exports = {
 	buildCImage: require("./buildCImage.js"),
 	createCContainer: require("./createCContainer.js"),
 	startCContainer: require("./startCContainer.js"),
+	copyClientFilesToCContainer: require("./copyClientFilesToCContainer"),
 	compileInCContainer: require("./compileInCContainer.js"),
 	execInCContainer: require("./execInCContainer.js"),
 	removeCContainer: require("./removeCContainer.js"),
