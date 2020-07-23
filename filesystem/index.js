480a14fd015c07dd2b6fdc722c08c6f84766ff60
Roshan <nahsorad@gmail.com>
2020-07-23 23:04:16 +0545

    Export initDirectories func and cleanUpTempFiles func

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,3 +1,5 @@
 module.exports = {
+	initDirectories: require("./initDirectories.js"),
 	generateSubmissionFile: require("./generateSubmissionFile.js"),
+	cleanUpTempFiles: require("./cleanUpTempFiles.js"),
 };
