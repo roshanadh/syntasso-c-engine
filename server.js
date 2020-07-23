ac42bc6bc220e5c211ee863e8251301c6ab7102b
Roshan <nahsorad@gmail.com>
2020-07-23 19:24:11 +0545

    Use CORS and initiate socket connection

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const router = require("./routes/index.js");
-const { PORT } = require("./config.js");
+const {
+	PORT,
+	CLIENT_PROTOCOL,
+	CLIENT_HOST,
+	CLIENT_PORT,
+} = require("./config.js");
 
 const app = express();
 app.set("json spaces", 2);
+app.use(
+	cors({
+		credentials: true,
+		origin: `${CLIENT_PROTOCOL}://${CLIENT_HOST}:${CLIENT_PORT}`,
+	})
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
@@ -14,4 +26,9 @@ const server = app.listen(PORT, () =>
 	console.log(`Syntasso C Engine is listening on port ${PORT}...`)
 );
 
-module.exports = server;
+const socketInstance = new (require("./socket/socket"))(server);
+
+module.exports = {
+	server,
+	socketInstance,
+};
