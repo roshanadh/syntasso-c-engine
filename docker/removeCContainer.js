3a09d90b2783de7c99a84b72f965b8fa2f151aba
Roshan <nahsorad@gmail.com>
2020-07-23 19:50:14 +0545

    Log if no container was found to be removed

--- docker/removeCContainer.js
+++ docker/removeCContainer.js
@@ -1,9 +1,9 @@
 const { exec } = require("child_process");
 
-module.exports = (req, socketInstance) => {
+module.exports = socketId => {
 	return new Promise((resolve, reject) => {
 		try {
-			const containerName = req.body.socketId;
+			const containerName = socketId;
 
 			console.log(
 				`Removing any existing containers named ${containerName}...`
@@ -11,16 +11,22 @@ module.exports = (req, socketInstance) => {
 			exec(
 				`docker container rm ${containerName} --force`,
 				(error, stdout, stderr) => {
-					if (
-						error &&
-						!error.message.includes(
-							`No such container: ${containerName}`
-						)
-					) {
-						console.error(
-							`error while removing container ${containerName}: ${error}`
-						);
-						return reject(error);
+					if (error) {
+						if (
+							!error.message.includes(
+								`No such container: ${containerName}`
+							)
+						) {
+							console.error(
+								`error while removing container ${containerName}: ${error}`
+							);
+							return reject(error);
+						} else {
+							console.log(
+								`No container named ${containerName} was found to be removed.`
+							);
+							return resolve(stdout);
+						}
 					} else if (
 						stderr &&
 						!stderr.includes(`No such container: ${containerName}`)
