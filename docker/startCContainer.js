77b4655946e572bbf81b6116ea69748150cc6cc6
Roshan <nahsorad@gmail.com>
2020-07-23 19:28:21 +0545

    Emit docker-app-stdout event to socket & wrap in try...catch

--- docker/startCContainer.js
+++ docker/startCContainer.js
@@ -1,28 +1,45 @@
 const { exec } = require("child_process");
 
-module.exports = containerName => {
+module.exports = (req, socketInstance) => {
 	return new Promise((resolve, reject) => {
-		console.log(`Starting a C container named ${containerName}...`);
-		exec(
-			`docker container start ${containerName}`,
-			(error, stdout, stderr) => {
-				if (error) {
-					console.error(
-						`error while starting container ${containerName}: ${error}`
-					);
-					return reject(error);
-				} else if (stderr) {
-					console.error(
-						`stderr while starting container ${containerName}: ${stderr}`
-					);
-					return reject(error);
+		try {
+			const { socketId } = req.body;
+			const containerName = socketId;
+
+			console.log(`Starting a C container named ${containerName}...`);
+			socketInstance.instance.to(socketId).emit("docker-app-stdout", {
+				stdout: `Starting the C container...`,
+			});
+
+			exec(
+				`docker container start ${containerName}`,
+				(error, stdout, stderr) => {
+					if (error) {
+						console.error(
+							`error while starting container ${containerName}: ${error}`
+						);
+						return reject(error);
+					} else if (stderr) {
+						console.error(
+							`stderr while starting container ${containerName}: ${stderr}`
+						);
+						return reject(stderr);
+					}
+					if (stdout.trim() !== "")
+						console.log(
+							`stdout while starting container ${containerName}: ${stdout}`
+						);
+					socketInstance.instance
+						.to(socketId)
+						.emit("docker-app-stdout", {
+							stdout: `C container started`,
+						});
+					return resolve(stdout);
 				}
-				if (stdout.trim() !== "")
-					console.log(
-						`stdout while starting container ${containerName}: ${stdout}`
-					);
-				return resolve(stdout);
-			}
-		);
+			);
+		} catch (error) {
+			console.log(`error during startCContainer: ${error}`);
+			return reject(error);
+		}
 	});
 };
