f24f02588a936585ef6615e65f9ddcdb6522123f
Roshan <nahsorad@gmail.com>
2020-07-23 19:25:24 +0545

    Use switch case for socketValidator

--- controllers/submitController.js
+++ controllers/submitController.js
@@ -1,9 +1,22 @@
 const {
+	socketValidator,
 	codeValidator,
 	dockerConfigValidator,
 } = require("../middlewares/paramValidator.js");
 
 module.exports = (req, res, next) => {
+	switch (socketValidator(req)) {
+		case "no-socket":
+			return res.status(400).json({
+				error: "No socket ID provided",
+			});
+		case "unknown-socket":
+			return res.status(401).json({
+				error: "Socket ID not recognized",
+			});
+		default:
+			break;
+	}
 	if (!codeValidator(req))
 		return res.status(400).json({
 			error: "No code provided",
