14d2c2b48f78327fe1cff82d93b760821943b08f
Roshan <nahsorad@gmail.com>
2020-07-23 19:49:17 +0545

    Export SECRET_SESSION_KEY

--- config.js
+++ config.js
@@ -7,4 +7,7 @@ module.exports = {
 	CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
 	CLIENT_HOST: process.env.CLIENT_HOST,
 	CLIENT_PORT: process.env.CLIENT_PORT,
+	SECRET_SESSION_KEY: process.env.SECRET_SESSION_KEY,
+	REDIS_STORE_HOST: process.env.REDIS_STORE_HOST,
+	REDIS_STORE_PORT: process.env.REDIS_STORE_PORT,
 };
