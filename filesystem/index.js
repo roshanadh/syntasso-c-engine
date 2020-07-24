694b7827897571b8d85212450414aa9a2b21d2da
Roshan <nahsorad@gmail.com>
2020-07-24 14:58:51 +0545

    Export writeOutputToFile func

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	initDirectories: require("./initDirectories.js"),
 	generateSubmissionFile: require("./generateSubmissionFile.js"),
+	writeOutputToFile: require("./writeOutputToFile.js"),
 	cleanUpTempFiles: require("./cleanUpTempFiles.js"),
 };
