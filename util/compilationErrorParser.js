d517333aaf45b07df2ac9f4b690e4ad842a1df25
Roshan <nahsorad@gmail.com>
2020-09-07 14:21:49 +0545

    Parse compilation terminations due to misnamed headers

--- util/compilationErrorParser.js
+++ util/compilationErrorParser.js
@@ -1,4 +1,18 @@
+const compilationTerminationParser = error => {
+	const splitError = error.split(":");
+	const lineNumber = splitError[0];
+	const columnNumber = splitError[1];
+	const errorStack = splitError.slice(2).join(":");
+	return {
+		lineNumber,
+		columnNumber,
+		errorStack,
+	};
+};
+
 module.exports = (error, socketId) => {
+	// Parses compilation error, compilation terminations, and linker errors
+
 	/*
 	 * compilation error sample:
 	 * 	s-c7f938d272062af924.c: In function 'main':
@@ -24,16 +38,47 @@ module.exports = (error, socketId) => {
 		// trim code portion from errorMessage
 		errorMessage = errorMessage.split("\n")[0];
 
+		// if any error hasn't been thrown up to here, it's a ...
+		// ... compilation error, so no need to update errorType
 		return {
 			errorMessage,
 			lineNumber,
 			columnNumber,
 			errorStack,
 		};
-	} catch (error) {
-		console.error(`error in compilationErrorParser.js:`, error);
+	} catch (caughtError) {
+		// TODO:
+		// check if it's a Linker Error as some Linker Errors cannot be ...
+		// ... parsed as above and throws an error in the process
+		if (error.includes("undefined reference to")) {
+			// "undefined reference to" is one of the most common ...
+			// ... Linker Errors
+
+			// if it is a Linker Error, update errorType to say: linker-error
+
+			return {
+				newErrorType: "linker-error",
+				errorStack: error,
+			};
+		}
+
+		// check for any compilation terminations
+		if (error.includes("compilation terminated")) {
+			// this is an instance of compilation error
+			let {
+				lineNumber,
+				columnNumber,
+				errorStack,
+			} = compilationTerminationParser(error);
+			return {
+				lineNumber,
+				columnNumber,
+				errorStack,
+			};
+		}
+		console.error(`error in compilationErrorParser.js:`, caughtError);
 		return {
-			errorInParser: error,
+			errorInParser: caughtError,
 		};
 	}
 };
