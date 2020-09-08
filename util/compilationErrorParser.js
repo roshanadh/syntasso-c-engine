38e9a79c7fc9474db489c38c3968b3981ee25f7a
Roshan <nahsorad@gmail.com>
2020-09-08 12:12:46 +0545

    Parse linker-error in case lineNumber is NaN

--- util/compilationErrorParser.js
+++ util/compilationErrorParser.js
@@ -40,29 +40,59 @@ module.exports = (error, socketId) => {
 
 		// if any error hasn't been thrown up to here, it's a ...
 		// ... compilation error, so no need to update errorType
-		return {
-			errorMessage,
-			lineNumber,
-			columnNumber,
-			errorStack,
-		};
-	} catch (caughtError) {
-		// TODO:
-		// check if it's a Linker Error as some Linker Errors cannot be ...
-		// ... parsed as above and throws an error in the process
-		if (error.includes("undefined reference to")) {
-			// "undefined reference to" is one of the most common ...
-			// ... Linker Errors
 
-			// if it is a Linker Error, update errorType to say: linker-error
-
+		// if lineNumber or columnNumber are NaN, some error has occurred ...
+		// ... during parsing
+		if (isNaN(lineNumber) || isNaN(columnNumber)) {
+			/*
+			 * this may be due to a Linker Error
+			 * Linker Errors may look like:
+			 * s-bf060be7a5b6ff7a93.c:(.text+0xa): undefined reference to `Foo'
+			 * collect2: error: ld returned 1 exit status
+			 *
+			 * This Linker Error will be parsed with lineNumber = "(.text+0xa)" ...
+			 * ... and columnNumber =  undefined reference to `Foo'↵collect2"
+			 */
+			if (lineNumber.includes(".text+0x")) {
+				// it is a Linker Error
+				errorMessage = columnNumber.trim().split("\n")[0];
+				newErrorType = "linker-error";
+				console.dir({
+					errorMessage,
+				});
+				// returning a newErrorType key will indicate to dockerConfigController that ...
+				// ... a new error type was detected while parsing and it will override the ...
+				// ... default 'compilation-error' error type
+				return {
+					newErrorType,
+					errorMessage,
+					errorStack,
+				};
+			} else {
+				console.error(
+					"New error type detected while parsing in compilationErrorParser:",
+					error
+				);
+				return { errorStack };
+			}
+		} else {
 			return {
-				newErrorType: "linker-error",
-				errorStack: error,
+				errorMessage,
+				lineNumber,
+				columnNumber,
+				errorStack,
 			};
 		}
-
+	} catch (caughtError) {
 		// check for any compilation terminations
+		/*
+		 * compilation termination sample:
+		 * s-081e82ab6dc36db601.c:1:16: fatal error: stdio: No such file or directory
+		 *	#include<stdio>
+		 *        			^
+		 *	compilation terminated.
+		 *
+		 */
 		if (error.includes("compilation terminated")) {
 			// this is an instance of compilation error
 			let {
@@ -74,6 +104,30 @@ module.exports = (error, socketId) => {
 				lineNumber,
 				columnNumber,
 				errorStack,
+			};
+		}
+		// TODO:
+		// find an efficient way to detect Linker Errors
+		// "undefined reference to..." can be one of them
+
+		// check if it's a Linker Error as some Linker Errors cannot be ...
+		// ... parsed as above and throws an error in the process
+		if (error.includes("undefined reference to")) {
+			// "undefined reference to" is one of the most common ...
+			// ... Linker Errors
+			try {
+				errorMessage = error
+					.substring(error.indexOf("undefined reference to"))
+					.split("\n")[0];
+			} catch (err) {
+				console.error("Error while parsing errorMessage:", err);
+				errorMessage = null;
+			}
+			// if it is a Linker Error, update errorType to say: linker-error
+			return {
+				newErrorType: "linker-error",
+				errorMessage,
+				errorStack: error,
 			};
 		}
 		console.error(`error in compilationErrorParser.js:`, caughtError);
