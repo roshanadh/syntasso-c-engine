f163e2d7772a6258767d9d2b7af1737078f3b1f5
Roshan <nahsorad@gmail.com>
2020-09-13 12:19:07 +0545

    Generate files for individual test cases

---
+++ filesystem/generateTestFiles.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = req => {
+	return new Promise(async (resolve, reject) => {
+		const { socketId, testCases } = req.body;
+		const sampleInputsDirPath = path.resolve(
+			__dirname,
+			"..",
+			"client-files",
+			socketId,
+			"sampleInputs"
+		);
+		const expectedOutputsDirPath = path.resolve(
+			__dirname,
+			"..",
+			"client-files",
+			socketId,
+			"expectedOutputs"
+		);
+
+		console.log(`Generating test case files for socket ID ${socketId}...`);
+		fs.mkdir(sampleInputsDirPath, { recursive: true }, err => {
+			if (err) {
+				// do nothing if path already exists
+				if (err.code === "EEXIST") {
+				} else {
+					reject(false);
+					throw err;
+				}
+			}
+			fs.mkdir(expectedOutputsDirPath, { recursive: true }, err => {
+				if (err) {
+					// do nothing if path already exists
+					if (err.code === "EEXIST") {
+					} else {
+						reject(false);
+						throw err;
+					}
+				}
+
+				// at this point, both sampleInputs and expectedOutputs dirs have ...
+				// ... been created, so write files inside the directories
+				let sampleInputFilePath, expectedOutputFilePath;
+				testCases.forEach((element, index) => {
+					sampleInputFilePath = path.resolve(
+						sampleInputsDirPath,
+						`${socketId}-sampleInput-${index}.txt`
+					);
+					expectedOutputFilePath = path.resolve(
+						expectedOutputsDirPath,
+						`${socketId}-expectedOutput-${index}.txt`
+					);
+					try {
+						fs.writeFileSync(
+							sampleInputFilePath,
+							element.sampleInput
+						);
+						console.log(
+							`${socketId}-sampleInput-${index}.txt generated.`
+						);
+						fs.writeFileSync(
+							expectedOutputFilePath,
+							element.expectedOutput
+						);
+						console.log(
+							`${socketId}-expectedOutput-${index}.txt generated.`
+						);
+					} catch (err) {
+						reject(err);
+						throw err;
+					}
+				});
+				console.log(
+					`Test case files generated for socket ID ${socketId}.`
+				);
+				return resolve(true);
+			});
+		});
+	});
+};
