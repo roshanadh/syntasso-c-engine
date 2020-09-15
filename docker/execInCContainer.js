884048b43fe70bf524caefe14ad6018c1a8706ec
Roshan <nahsorad@gmail.com>
2020-09-15 14:50:13 +0545

    Fix JSON parse error for stderr

--- docker/execInCContainer.js
+++ docker/execInCContainer.js
@@ -64,7 +64,7 @@ module.exports = (req, socketInstance) => {
 				}
 			});
 			mainWrapper.stderr.on("data", stderr => {
-				stderr = JSON.parse(stderr);
+				stderr = JSON.parse(stderr.toString());
 				console.error(
 					`stderr while executing submission inside container ${containerName}:`,
 					stderr.stderr
