40b5997ae05d0ba0fc824370675b5faa4292747f
Roshan <nahsorad@gmail.com>
2020-09-15 19:58:05 +0545

    Use try...catch block to prevent undetected errors
    Before this commit, errors like 'testCases.forEach is not a function' passed undetected

--- filesystem/generateTestFiles.js
+++ filesystem/generateTestFiles.js
@@ -3,34 +3,30 @@ const path = require("path");
 
 module.exports = req => {
 	return new Promise(async (resolve, reject) => {
-		const { socketId, testCases } = req.body;
-		await require("./removeTestFiles.js")(socketId);
-		const sampleInputsDirPath = path.resolve(
-			__dirname,
-			"..",
-			"client-files",
-			socketId,
-			"sampleInputs"
-		);
-		const expectedOutputsDirPath = path.resolve(
-			__dirname,
-			"..",
-			"client-files",
-			socketId,
-			"expectedOutputs"
-		);
+		const { socketId } = req.body;
+		let { testCases } = req.body;
+		try {
+			testCases = JSON.parse(testCases);
+			await require("./removeTestFiles.js")(socketId);
+			const sampleInputsDirPath = path.resolve(
+				__dirname,
+				"..",
+				"client-files",
+				socketId,
+				"sampleInputs"
+			);
+			const expectedOutputsDirPath = path.resolve(
+				__dirname,
+				"..",
+				"client-files",
+				socketId,
+				"expectedOutputs"
+			);
 
-		console.log(`Generating test case files for socket ID ${socketId}...`);
-		fs.mkdir(sampleInputsDirPath, { recursive: true }, err => {
-			if (err) {
-				// do nothing if path already exists
-				if (err.code === "EEXIST") {
-				} else {
-					reject(false);
-					throw err;
-				}
-			}
-			fs.mkdir(expectedOutputsDirPath, { recursive: true }, err => {
+			console.log(
+				`Generating test case files for socket ID ${socketId}...`
+			);
+			fs.mkdir(sampleInputsDirPath, { recursive: true }, err => {
 				if (err) {
 					// do nothing if path already exists
 					if (err.code === "EEXIST") {
@@ -39,44 +35,71 @@ module.exports = req => {
 						throw err;
 					}
 				}
-
-				// at this point, both sampleInputs and expectedOutputs dirs have ...
-				// ... been created, so write files inside the directories
-				let sampleInputFilePath, expectedOutputFilePath;
-				testCases.forEach((element, index) => {
-					sampleInputFilePath = path.resolve(
-						sampleInputsDirPath,
-						`${socketId}-sampleInput-${index}.txt`
-					);
-					expectedOutputFilePath = path.resolve(
-						expectedOutputsDirPath,
-						`${socketId}-expectedOutput-${index}.txt`
-					);
-					try {
-						fs.writeFileSync(
-							sampleInputFilePath,
-							element.sampleInput.toString()
-						);
-						console.log(
-							`${socketId}-sampleInput-${index}.txt generated.`
-						);
-						fs.writeFileSync(
-							expectedOutputFilePath,
-							element.expectedOutput.toString()
-						);
-						console.log(
-							`${socketId}-expectedOutput-${index}.txt generated.`
-						);
-					} catch (err) {
-						reject(err);
-						throw err;
+				fs.mkdir(expectedOutputsDirPath, { recursive: true }, err => {
+					if (err) {
+						// do nothing if path already exists
+						if (err.code === "EEXIST") {
+						} else {
+							reject(false);
+							throw err;
+						}
 					}
+
+					// at this point, both sampleInputs and expectedOutputs dirs have ...
+					// ... been created, so write files inside the directories
+					let sampleInputFilePath, expectedOutputFilePath;
+					try {
+						testCases.forEach((element, index) => {
+							sampleInputFilePath = path.resolve(
+								sampleInputsDirPath,
+								`${socketId}-sampleInput-${index}.txt`
+							);
+							expectedOutputFilePath = path.resolve(
+								expectedOutputsDirPath,
+								`${socketId}-expectedOutput-${index}.txt`
+							);
+							try {
+								fs.writeFileSync(
+									sampleInputFilePath,
+									element.sampleInput.toString()
+								);
+								console.log(
+									`${socketId}-sampleInput-${index}.txt generated.`
+								);
+								fs.writeFileSync(
+									expectedOutputFilePath,
+									element.expectedOutput.toString()
+								);
+								console.log(
+									`${socketId}-expectedOutput-${index}.txt generated.`
+								);
+							} catch (err) {
+								console.error(
+									`Error while writing to test case files for socketId ${socketId}:`,
+									err
+								);
+								return reject(err);
+							}
+						});
+					} catch (err) {
+						console.error(
+							`Error while writing to test case files for socketId ${socketId}:`,
+							err
+						);
+						return reject(err);
+					}
+					console.log(
+						`Test case files generated for socket ID ${socketId}.`
+					);
+					return resolve(true);
 				});
-				console.log(
-					`Test case files generated for socket ID ${socketId}.`
-				);
-				return resolve(true);
 			});
-		});
+		} catch (error) {
+			console.error(
+				`Error while writing to test case files for socketId ${socketId}:`,
+				error
+			);
+			return reject(error);
+		}
 	});
 };
