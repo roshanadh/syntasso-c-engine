db46544f4086a6672c734ab4b3e38ac697ec214a
Roshan <nahsorad@gmail.com>
2020-09-18 19:35:40 +0545

    Resolve time taken to start container

--- docker/startCContainer.js
+++ docker/startCContainer.js
@@ -1,5 +1,7 @@
 const { exec } = require("child_process");
 
+const convertTimeToMs = require("../util/convertTimeToMs.js");
+
 module.exports = (req, socketInstance) => {
 	return new Promise((resolve, reject) => {
 		/*
@@ -17,13 +19,15 @@ module.exports = (req, socketInstance) => {
 			const { socketId } = req.body;
 			const containerName = socketId;
 
+			let containerStartTime;
 			console.log(`Starting a C container named ${containerName}...`);
 			socketInstance.instance.to(socketId).emit("docker-app-stdout", {
 				stdout: `Starting the C container...`,
 			});
 
 			exec(
-				`docker container start ${containerName}`,
+				`time docker container start ${containerName}`,
+				{ shell: "/bin/bash" },
 				(error, stdout, stderr) => {
 					if (error) {
 						console.error(
@@ -32,11 +36,38 @@ module.exports = (req, socketInstance) => {
 						);
 						return reject({ error });
 					} else if (stderr) {
-						console.error(
-							`stderr while starting container ${containerName}:`,
-							stderr
-						);
-						return reject({ stderr });
+						let times;
+						/*
+						 * 'time' command returns the real(total), user, and sys(system) ...
+						 * ... times for the execution of following command (e.g. docker build ... )
+						 * The times are returned in the following structure:
+						 * ++++++++++++++++++
+						 * + real\t0m0.000s +
+						 * + user\t0m0.000s +
+						 * + sys\t0m0.000s  +
+						 * ++++++++++++++++++
+						 * Note: 0m0.000s = 0minutes and 0.000 seconds
+						 * We need to extract real(total) time/containerStartTime from the returned timed.
+						 * The times are returned as an 'stderr' object
+						 */
+						try {
+							times = stderr.split("\n");
+							// get build time in terms of 0m.000s
+							containerStartTime = times[1].split("\t")[1];
+							return resolve({
+								stdout,
+								containerStartTime: convertTimeToMs(
+									containerStartTime
+								),
+							});
+						} catch (err) {
+							// stderr contains an actual error and not execution times
+							console.error(
+								`stderr while starting container ${containerName}:`,
+								stderr
+							);
+							return reject({ stderr });
+						}
 					}
 					if (stdout.trim() !== "")
 						console.log(
