eb15ab98b2a417d4abe1704576a022f5353b6a71
Roshan <nahsorad@gmail.com>
2020-10-12 12:55:09 +0545

    Export a func to link object file created by compileInCContainer

---
+++ docker/linkInCContainer.js
@@ -0,0 +1,77 @@
+const { exec } = require("child_process");
+const { performance } = require("perf_hooks");
+
+module.exports = (req, socketInstance) => {
+	return new Promise((resolve, reject) => {
+		try {
+			let linkTime = performance.now();
+			const { socketId } = req.body;
+			console.log("Linking the object file inside container...");
+			socketInstance.instance.to(socketId).emit("docker-app-stdout", {
+				stdout: "Linking the object file inside container...",
+			});
+			exec(
+				`docker exec -i ${socketId} gcc ${socketId}.o -o submission`,
+				(error, stdout, stderr) => {
+					linkTime = performance.now() - linkTime;
+					if (stderr) {
+						console.error(
+							`stderr while linking ${socketId}.o:`,
+							stderr
+						);
+						socketInstance.instance
+							.to(socketId)
+							.emit("docker-app-stdout", {
+								stdout: `stderr while linking object file: ${stderr}`,
+							});
+						/*
+						 * reject an object with key 'linkerError' because it makes distinguishing the ...
+						 * ... type of error easier when handling promise rejections inside submitController
+						 */
+						return reject({ linkerError: stderr, linkTime });
+					}
+					if (error) {
+						console.error(
+							`Error while linking ${socketId}.o:`,
+							error
+						);
+						socketInstance.instance
+							.to(socketId)
+							.emit("docker-app-stdout", {
+								stdout: `Error while linking object file: ${stderr}`,
+							});
+						/*
+						 * reject an object with key 'linkerError' because it makes distinguishing the ...
+						 * ... type of error easier when handling promise rejections inside submitController
+						 */
+						return reject({
+							linkerError: error,
+							linkTime,
+						});
+					}
+					// at this point, linking has completed without any errors ...
+					if (stdout) {
+						console.log(
+							`stdout during linking of object file: ${stdout}`
+						);
+						socketInstance.instance
+							.to(socketId)
+							.emit("docker-app-stdout", {
+								stdout: `stdout during linking of object file: ${stdout}`,
+							});
+					}
+					console.log(`${socketId}.o file linked.`);
+					socketInstance.instance
+						.to(socketId)
+						.emit("docker-app-stdout", {
+							stdout: `${socketId}.o file linked.`,
+						});
+					return resolve({ stdout, linkTime });
+				}
+			);
+		} catch (error) {
+			console.error("Error in linkInCContainer:", error);
+			return reject({ error });
+		}
+	});
+};
