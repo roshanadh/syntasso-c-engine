3ff981c1554f41df5501b094e5092d3b62c67c86
Roshan <nahsorad@gmail.com>
2020-10-23 16:33:53 +0545

    Test for container initialization on socket connection

--- test/1-connection.test.js
+++ test/1-connection.test.js
@@ -1,16 +1,19 @@
 const { mocha, chai, should, expect, server } = require("./test-config.js");
 
-describe("Test connection:", () => {
-	let socket;
+describe("Tests: ", () => {
+	let socket, socketId;
 	before(async () => {
 		const { createConnection } = require("./test-config.js");
 		socket = await createConnection();
 	});
 
-	describe("Test socket connection to http://localhost:8081", () => {
+	describe("Test socket connection at http://localhost:8081", () => {
 		it("should be connected to a socket", done => {
 			expect(socket.connected).to.be.true;
-			done();
+			socket.on("container-ready-status", logs => {
+				expect(logs.status).to.equal("ready");
+				done();
+			});
 		});
 	});
 });
