fbb7f9f99befb94c0c3a49ca911d77ed15d4c699
Roshan <nahsorad@gmail.com>
2021-03-31 12:45:29 +0545

    Fill processes array with elements with compilationError info

---
+++ util/fillProcessesArray.js
@@ -0,0 +1,16 @@
+module.exports = (compilationError, testCasesCount) => {
+	let processes = [];
+	for (let i = 0; i < testCasesCount; i++) {
+		processes.push({
+			id: i,
+			testStatus: false,
+			timedOut: false,
+			sampleInput: null,
+			expectedOutput: null,
+			exception: null,
+			observedOutputTooLong: false,
+			executionTimeForProcess: null,
+			compilationError,
+		});
+	}
+};
