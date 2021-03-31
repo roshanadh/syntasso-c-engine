1db9d7883835b3bc810e97678767393378788161
Roshan <nahsorad@gmail.com>
2021-03-31 13:16:40 +0545

    Add doc string

--- util/fillProcessesArray.js
+++ util/fillProcessesArray.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * @param {*} parsedCompilationError
+ * @param {*} testCasesCount
+ * @returns processes[]
+ *
+ * Fill processes array with n process JSON objects, for n test cases
+ * Each process JSON object contains the parsed compilation error object, among others
+ */
 module.exports = (parsedCompilationError, testCasesCount) => {
 	let processes = [];
 	for (let i = 0; i < testCasesCount; i++) {
